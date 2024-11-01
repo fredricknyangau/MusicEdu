2a3920b492c29c3bb0d58eed50720ef3d9319faf
fredricknyangau <nyangaufredrick443@gmail.com>
2024-11-01 08:57:30 +0300

    deleted:    public/add-instrument.html 	deleted:    public/edit-instrument.html 	deleted:    public/instrument.html 	deleted:    public/js/add-instrument.js 	modified:   routes/instrumentRoutes.js

--- routes/instrumentRoutes.js
+++ routes/instrumentRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Instrument = require('../models/Instrument');
+const Instrument = require('../models/instruments');
 const multer = require('multer');
 const path = require('path');
 

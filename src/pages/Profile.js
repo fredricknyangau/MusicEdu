b266037291b2fe1c8dfdbc193f6668d13e990600
fredricknyangau <nyangaufredrick443@gmail.com>
2025-03-29 12:28:18 +0300

    updated

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -53,7 +53,7 @@ const Profile = () => {
 
             setLoading(true);
             try {
-                const response = await fetch('http://localhost:5000/api/users/profile', {
+                const response = await fetch('https://music-edu-backend.vercel.app/api/users/profile', {
                     method: 'GET',
                     headers: {'Authorization': `Bearer ${token}`}
                 });
@@ -95,7 +95,7 @@ const Profile = () => {
 
         setLoading(true);
         try {
-            const response = await fetch('http://localhost:5000/api/users/profile', {
+            const response = await fetch('https://music-edu-backend.vercel.app/api/users/profile', {
                 method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json',
